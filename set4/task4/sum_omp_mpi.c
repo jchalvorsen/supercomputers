e500eb8a9f9ac88d50555739f9c80927c58f3c3b
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2015-02-17 19:34:43 +0100

    fixed task 4

--- set4/task4/sum_omp_mpi.c
+++ set4/task4/sum_omp_mpi.c
@@ -1,25 +1,78 @@
+#include <stdlib.h>
 #include <stdio.h>
-//#include "mpi.h"
-//#include <omp.h>
+#include <math.h>
+
 #include "common.h"
 
-int main(int argc, char *argv[]) {
-  int numprocs, rank, namelen;
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int iam = 0, np = 1;
+Vector generateV(n){
+    Vector v = createVector(n);//MPI(n, &WorldComm,1);
+    double myNum;
+    int i;
+    for (i=1;i<v->len+1;++i){
+        myNum = 1/(i*i*1.0);
+        v->data[i-1] = myNum;
+    }
+    return v;
+}
 
+
+int main(int argc, char **argv)
+{ 
+  // Define variables
+  int n, rank, size, i;
+  double S = M_PI*M_PI/6;
+  double sum, t1, t2, dt, error;
+  Vector v;
+  int *len, *displ;
+  double *myData;
+    
   MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Get_processor_name(processor_name, &namelen);
-
-  #pragma omp parallel default(shared) private(iam, np)
-  {
-    np = omp_get_num_threads();
-    iam = omp_get_thread_num();
-    printf("Hello from thread %d out of %d from process %d out of %d on %s\n",
-           iam, np, rank, numprocs, processor_name);
+  
+  int k;
+  for (k = 3; k<=14; ++k){
+  n = pow(2,k);
+  int elements_per_proc = n/size;
+  if (rank == 0) {    
+    //n = elements_per_proc*size;
+    v = generateV(n);
+    myData = v->data;
+    t1 = MPI_Wtime();
   }
+  
+  // buffer for data
+  double *privateData = malloc(sizeof(double) * elements_per_proc);
+  MPI_Scatter(myData, elements_per_proc, MPI_DOUBLE, privateData,
+            elements_per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+  // Compute the sum
+  sum = 0.0;
+  #pragma omp parallel for schedule(static) reduction(+:sum)  
+  for (i = 0; i < elements_per_proc; ++i) {
+     sum = sum + privateData[i];
+  }
+  
+  double *ourSum = NULL;
+  if (rank == 0) {
+    ourSum = malloc(sizeof(double) * size);
+  }
+  
+  MPI_Gather(&sum, 1, MPI_DOUBLE, ourSum, 1, MPI_DOUBLE, 0,
+           MPI_COMM_WORLD);
+
+
+  if (rank == 0) {
+    double allSums = 0;
+    for (i = 0; i < size; ++i) {
+      allSums = allSums + ourSum[i];
+    }
+    t2 = MPI_Wtime();
+    dt = t2 - t1;
+    error = fabs(S-allSums);
+    printf ("sum= %f error= %f dt= %f n=%d \n", allSums, error, dt, n);
+  }
+  }
   MPI_Finalize();
+  return 0;
 }
