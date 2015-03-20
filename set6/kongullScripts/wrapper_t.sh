bf256099b1dd24751bf9b6355a4c2868f044f0f4
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2015-03-20 12:35:51 +0100

    Added scripts to run on kongull

---
+++ set6/kongullScripts/wrapper_t.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Name job 'poisson'
+#PBS -N wrapper_jc
+
+# Allocate two nodes with 12 processors from the default resources
+#PBS -lnodes=1:ppn=1:default
+
+# Expect to run up to 5 minutes
+#PBS -lwalltime=00:1:00
+
+# Memory per process
+#PBS -lpmem=2000MB
+
+# Run on the freecycle account
+#PBS -A freecycle
+
+# Run in the optimist queue by default
+#PBS -q optimist
+
+# Join stdout and stderr output to one file
+#PBS -j oe
+
+# Change directory to dir with the job script
+#cd ${PBS_O_WORKDIR}
+
+# prepare output file
+echo "n, np, nt, maxError, used time" > out.txt
+
+
+
+# Run with differing amounts of p and t st p*t = 36
+qsub -t 1,2,3,4,6,9,12,18,36 job_varyt.sh
+
