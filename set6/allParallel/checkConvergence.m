d1bb551e32f8404fcc9107875bc47a5860819e2a
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2015-03-20 10:31:57 +0100

    Addruntime plot

--- set6/allParallel/checkConvergence.m
+++ set6/allParallel/checkConvergence.m
@@ -1,9 +1,6 @@
 close all
 clear all
 
-errors = [1.927711e-01 , 4.476185e-02, 1.098931e-02 , 2.734955e-03 , 6.829684e-04 , 1.706940e-04 , 4.267049e-05, 1.066744e-05, 2.666847e-06 ];
-ns = [4, 8, 16, 32, 64, 128, 256, 512, 1024];
-
 Tn = sortrows(readtable('varyn.txt'))
 ns = Tn{:,1};
 errors = Tn{:,4};
@@ -18,11 +15,19 @@ ylabel('max error')
 
 
 figure
-Tp = sortrows(readtable('varyp.txt'))
-plot(Tp{:,2},Tp{:,5})
-n = Tp{1,1};
+Tt = sortrows(readtable('varyp.txt'))
+plot(Tt{:,2},Tt{:,5})
+n = Tt{1,1};
 xlabel('Number of processors')
 ylabel('Runtime in seconds')
 string = sprintf('Runtime for n = %d \n with p*t = 36', n);
 legend(string)
 
+figure
+plot(Tn{:,1}, Tn{:,5})
+ylabel('Runtime in seconds')
+xlabel('Problem size n')
+string2 = sprintf('Runtime with p = %d and t = %d', Tn{1,2}, Tn{1,3});
+legend(string2);
+
+% Task d - Report speedup
