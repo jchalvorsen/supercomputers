c68c1c0e773e02f582347ae4d79eea6ba69c78d6
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2015-03-20 10:11:56 +0100

    Automate

--- set6/allParallel/checkConvergence.m
+++ set6/allParallel/checkConvergence.m
@@ -4,7 +4,9 @@ clear all
 errors = [1.927711e-01 , 4.476185e-02, 1.098931e-02 , 2.734955e-03 , 6.829684e-04 , 1.706940e-04 , 4.267049e-05, 1.066744e-05, 2.666847e-06 ];
 ns = [4, 8, 16, 32, 64, 128, 256, 512, 1024];
 
-
+Tn = sortrows(readtable('varyn.txt'))
+ns = Tn{:,1};
+errors = Tn{:,4};
 
 loglog(ns, errors)
 hold on
@@ -13,3 +15,14 @@ loglog(ns, 1./ns)
 legend('Our data','quadratic convergence','linear convergence') 
 xlabel('problem size n')
 ylabel('max error')
+
+
+figure
+Tp = sortrows(readtable('varyp.txt'))
+plot(Tp{:,2},Tp{:,5})
+n = Tp{1,1};
+xlabel('Number of processors')
+ylabel('Runtime in seconds')
+string = sprintf('Runtime for n = %d \n with p*t = 36', n);
+legend(string)
+
